71a1f20dfbec59db79a111afc51cf9ff29e5c1f3
Jude Bobinihi <jude.bobinihi@sovtech.com>
2022-07-17 12:30:21 +0200

    feat (getstream): initial getstream config & patch

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {OverlayProvider} from 'stream-chat-react-native';
+
 const App = () => {
   return (
-    <SafeAreaView>
-      <View style={{alignItems: 'center'}}>
-        <Text style={{fontWeight: 'bold'}}>CHAT APP </Text>
-      </View>
-    </SafeAreaView>
+    <GestureHandlerRootView>
+      <OverlayProvider>
+        <SafeAreaView>
+          <View style={{alignItems: 'center'}}>
+            <Text style={{fontWeight: 'bold'}}>CHAT APP </Text>
+          </View>
+        </SafeAreaView>
+      </OverlayProvider>
+    </GestureHandlerRootView>
   );
 };
 
